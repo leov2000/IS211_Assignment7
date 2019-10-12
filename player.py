f10aa3edc2f0f03cd6d37bb19a4c4b2123de860f
Leonel Vergara <leovergarah@gmail.com>
2019-10-11 20:08:20 -0400

    (Added) basic boilerplate methods for Player class

---
+++ player.py
@@ -0,0 +1,22 @@
+import uuid
+
+class Player:
+    def __init__(self, name = uuid.uuid4().hex):
+        self.score = 0
+        self.name = name
+        self.tally = []
+
+    def empty_score_tally(self):
+        self.tally = []
+    
+    def push_to_score_tally(self):
+        return self.tally
+    
+    def add_player_score(self, score_num):
+        self.score = self.score + score_num
+        
+    def get_player_score(self):
+        return self.score
+
+    def get_player_name(self):
+        return self.name  
