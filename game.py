8b8037f0fe365209bd3eeaa57b2ebeab06a8cbc1
Leonel Vergara <leovergarah@gmail.com>
2019-10-11 20:38:56 -0400

    (Added) basic boilerplate methods for Game  class

---
+++ game.py
@@ -0,0 +1,27 @@
+class Game:
+
+    def __init__(self, players):
+        self.players = players
+        self.die = None 
+        self.turn = None
+
+    def check_score(self, players):
+        for player in self.players:
+            if player.get_player_score() >= 100:
+                print("you won")
+                self.reset_game()
+
+
+    def reset_game(self):
+        for player in self.players:
+            player.reset_score()
+
+        self.die = None 
+        self.turn = None
+    
+    def roll_or_hold(self, player):
+        pass 
+        
+    def next_turn(self, player):
+        self.turn = player 
+
