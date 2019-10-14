16351db494411f399d8c91f2143e563606e24946
Leonel Vergara <leovergarah@gmail.com>
2019-10-14 16:14:59 -0400

    (Added) Doc string to Die Piece Class

--- die.py
+++ die.py
@@ -6,12 +6,29 @@ class Die:
         self.roll_count = 0
 
     def reset_die_side(self):
+        """
+        A function delegated to reseting the side state
+        
+        Parameters: None
+        """
         self.side = 0
     
     def reset_roll_count(self):
+        """
+        A function delegated to reseting the count state
+        
+        Parameters: None
+        """
         self.roll_count = 0
     
     def roll_the_die(self):
+        """
+        A function delegated to setting the roll and count state
+        
+        Parameters: None
+
+        Returns: a random int from 1-6.
+        """
         random_num = randint(1,6)
 
         self.side = random_num
@@ -20,4 +37,9 @@ class Die:
         return self.side
     
     def get_die_state(self):
+        """
+        A function deleted to retrieving the side and count state of the die.
+
+        Parameters: None
+        """
         return (self.side, self.roll_count)
